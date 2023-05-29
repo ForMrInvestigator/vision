4ff45cfd35e28af4aa9ebd13ce1c7c93f5d96a33
taylorhere <taylorherelee@gmail.com>
2023-05-29 18:58:32 +0800

    add process

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM registry.cn-shanghai.aliyuncs.com/invs/pytorch:23.04-py3
-WORKDIR /app/vision
-ADD . .
+FROM nvcr.io/nvidia/pytorch:23.04-py3
+RUN apt-get -y update && apt-get -y upgrade && DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC \
+    apt-get install -y --no-install-recommends \
+    ffmpeg
+RUN pip install labelme
---
+++ references/classification/process.py
@@ -0,0 +1,200 @@
+import os
+import random
+import shutil
+import labelme
+import PIL
+import io
+import subprocess
+import json
+
+
+def preprocess(
+    raw_video_basedir,
+    frame_output_basedir,
+    mobilenet_train_data_output_basedir,
+    merges=[],
+):
+    videos = []
+    for raw_file in os.listdir(raw_video_basedir):
+        raw_file_abs = os.sep.join([raw_video_basedir, raw_file])
+        if (
+            os.path.isfile(raw_file_abs)
+            and not raw_file.startswith(".")
+            and (raw_file.endswith(".mp4") or raw_file.endswith(".MOV"))
+        ):
+            ffprobe = [
+                "ffprobe",
+                "-v",
+                "error",
+                "-select_streams",
+                "v:0",
+                "-count_frames",
+                "-show_entries",
+                "stream=nb_read_frames,avg_frame_rate",
+                "-of",
+                "json",
+                raw_file_abs,
+            ]
+            print(" ".join(ffprobe))
+            process = subprocess.Popen(
+                ffprobe, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            out, err = process.communicate()
+            if err:
+                print(err, process.returncode)
+            video_info = json.loads(out)["streams"][0]
+            frames = int(video_info["nb_read_frames"])
+            frame_rate = video_info["avg_frame_rate"].split("/")
+            frame_rate = float(frame_rate[0]) / float(frame_rate[1])
+            videos.append((raw_file_abs, frames, frame_rate))
+
+    _, shortest, _ = sorted(videos, key=lambda x: x[1])[0]
+    # shortest = 100
+    videos = sorted(videos, key=lambda x: x[0])
+    names = {}
+    i = -1
+    for raw_file_abs, _, _ in videos:
+        short_name = raw_file_abs.split("/")[-1].split(".")[0]
+        i += 1
+        names[short_name] = i
+        for merge in merges:
+            if short_name in merge:
+                merge_name = merge[0]
+                a = names.get(merge_name, i)
+                print(short_name, merge_name, a)
+                names[short_name] = a
+                i = a
+
+    for raw_file_abs, frames, frame_rate in videos:
+        short_name = raw_file_abs.split("/")[-1].split(".")[0]
+        merged_name = str(names[short_name])
+        outputpath = os.sep.join([frame_output_basedir, merged_name])
+        outputfile = os.sep.join([outputpath, f"{short_name}_%d.png"])
+        print(frames, shortest, frame_rate)
+        ffmpeg = [
+            "ffmpeg",
+            "-i",
+            raw_file_abs,
+            "-s",
+            "360x640",
+            "-r",
+            str(shortest / frames * frame_rate),
+            outputfile,
+        ]
+        if not os.path.exists(outputpath):
+            os.makedirs(outputpath)
+        print(" ".join(ffmpeg))
+        process = subprocess.Popen(
+            ffmpeg, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        out, err = process.communicate()
+        if err:
+            print(err.decode(), process.returncode)
+
+    for klass in os.listdir(frame_output_basedir):
+        klass_abs = os.sep.join([frame_output_basedir, klass])
+        if (
+            os.path.isdir(klass_abs)
+            and not klass.startswith(".")
+            and (klass != "val" or klass != "train")
+        ):
+            targets = []
+            for target in os.listdir(klass_abs):
+                target_abs = os.sep.join([klass_abs, target])
+                if os.path.isfile(target_abs) and target.endswith(".json"):
+                    target = labelfile.filename.replace(".json", ".jpg")
+                    if os.path.exists(target):
+                        continue
+                    labelfile = labelme.LabelFile(target)
+                    image = PIL.Image.open(io.BytesIO(labelfile.imageData)).convert(
+                        "RGB"
+                    )
+                    image.save(target)
+                    targets.append(target)
+                if (
+                    os.path.isfile(target_abs)
+                    and not target.startswith(".")
+                    and (
+                        target.lower().endswith(".png")
+                        or target.lower().endswith(".jpg")
+                        or target.lower().endswith(".jpeg")
+                    )
+                ):
+                    targets.append(target_abs)
+            vals = random.sample(targets, int(len(targets) * 0.3))
+            trains = set(targets) - set(vals)
+
+            val_klass_path = os.sep.join(
+                [mobilenet_train_data_output_basedir, "val", klass]
+            )
+            if not os.path.exists(val_klass_path):
+                os.makedirs(val_klass_path)
+
+            for val_abs in vals:
+                shutil.copy(
+                    val_abs, os.sep.join([val_klass_path, val_abs.split(os.sep)[-1]])
+                )
+
+            train_klass_path = os.sep.join(
+                [mobilenet_train_data_output_basedir, "train", klass]
+            )
+            if not os.path.exists(train_klass_path):
+                os.makedirs(train_klass_path)
+
+            for train_abs in trains:
+                shutil.copy(
+                    train_abs,
+                    os.sep.join([train_klass_path, train_abs.split(os.sep)[-1]]),
+                )
+    print(names)
+
+
+def postprocess(checkpoint_pth, export_file):
+    # model = torchvision.models.get_model("mobilenet_v2", num_classes=1000)
+    model = torchvision.models.get_model("mobilenet_v2", weights=weights)
+    checkpoint = torch.load(
+        checkpoint_pth,
+        map_location="cpu",
+    )
+    model.load_state_dict(checkpoint["model"])
+    torch.onnx.export(
+        model,
+        torch.rand((1, 3, 224, 224)),
+        export_file,
+        input_names=["input"],
+        output_names=["output"],
+    )
+
+
+def get_args_parser(add_help=True):
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="pre and post process", add_help=add_help
+    )
+
+    parser.add_argument("--raw_video_basedir", type=str)
+    parser.add_argument("--frame_output_basedir", type=str)
+    parser.add_argument("--mobilenet_train_data_output_basedir", type=str)
+    parser.add_argument("--merges", default=[], action="append", nargs="+")
+    parser.add_argument("--checkpoint_pth", type=str)
+    parser.add_argument("--export_file", type=str)
+    parser.add_argument(
+        "--mode", default="pre", type=str, choices=["pre", "post"]
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_args_parser().parse_args()
+    print(args)
+    if args.mode == "pre":
+        preprocess(
+            raw_video_basedir=args.raw_video_basedir,
+            frame_output_basedir=args.frame_output_basedir,
+            mobilenet_train_data_output_basedir=args.mobilenet_train_data_output_basedir,
+            merges=args.merges
+        )
+    elif args.mode == "post":
+        postprocess(checkpoint_pth=args.checkpoint_pth, export_file=args.export_file)
